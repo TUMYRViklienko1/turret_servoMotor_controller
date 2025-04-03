f113c8bb7991a71740ef5116e01dd5b26ca44d82
fish <timyrviklienko@gmail.com>
2025-04-04 00:59:11 +0200

    Refactor servo control implementation and add ServoSmooth library for improved performance

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,22 @@
 #include <iarduino_MultiServo.h>
 #include <SPI.h>
 #include <math.h>
-
+#include <ServoDriverSmooth.h>
 
 constexpr uint16_t SERVO_MIN = 130;
 constexpr uint16_t SERVO_MAX = 470;
-constexpr uint16_t INITIAL_ANGLE = (SERVO_MIN + SERVO_MAX)/2;
-constexpr uint16_t MAX_ANGLE = 180;
+constexpr uint16_t INITIAL_ANGLE = 90;
 constexpr uint8_t MAX_SERVO = 2;
 constexpr uint8_t HEAD_DEGREE = 2;
 constexpr uint16_t PWM_RESOLUTION = 4096;
 constexpr uint16_t SERVO_PERIOD_US = 20000;
 
-uint8_t baseServo = 0;     // Pin for pan
-uint8_t shoulderServo = 1; // Pin for tilt
+constexpr uint8_t BASESERVO = 0;     // Pin for pan
+constexpr uint8_t SHOULDERSERVO = 1; // Pin for tilt
 
-iarduino_MultiServo ServoController;
+uint32_t myTimer;
+
+ServoDriverSmooth ServoController[MAX_SERVO];
 uint16_t headCoordinates[HEAD_DEGREE] = {INITIAL_ANGLE, INITIAL_ANGLE};
 
 void getFromSerialPort();
@@ -24,27 +25,37 @@ void sendToServo();
 void convertToPWM(uint16_t* dataFromSerial);
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
 
     while (!Serial);
 
     // Initialize servo settings
-    ServoController.servoSet(baseServo, MAX_ANGLE, SERVO_MIN, SERVO_MAX);
-    ServoController.servoSet(shoulderServo, MAX_ANGLE, SERVO_MIN, SERVO_MAX);
-    ServoController.begin();
+    ServoController[BASESERVO].attach(BASESERVO, SERVO_MIN, SERVO_MAX,105);
+    ServoController[SHOULDERSERVO].attach(SHOULDERSERVO, SERVO_MIN, SERVO_MAX,130);
+
+
+    ServoController[BASESERVO].smoothStart();
+    ServoController[SHOULDERSERVO].smoothStart();
     
-    // Move servos to initial position
-    ServoController.analogWrite(baseServo, INITIAL_ANGLE);
-    ServoController.analogWrite(shoulderServo, INITIAL_ANGLE);
+    //ser speed limit
+    ServoController[BASESERVO].setSpeed(500);
+    ServoController[BASESERVO].setAccel(1500);
+    
+    ServoController[SHOULDERSERVO].setSpeed(500);
+    ServoController[SHOULDERSERVO].setAccel(1500);
+
 }
 
 void loop() {
-    
+
+    ServoController[BASESERVO].tick();
+    ServoController[SHOULDERSERVO].tick();
+
     if (Serial.available()) {
-        
         getFromSerialPort();
         sendToServo();
     }
+
 }
 
 void getFromSerialPort() {
@@ -64,6 +75,8 @@ void sendToServo() {
     int tiltAngle = (long(headCoordinates[1]) * PWM_RESOLUTION) / SERVO_PERIOD_US;
     
     Serial.println(panAngle);
-    ServoController.analogWrite(baseServo, panAngle);
-    ServoController.analogWrite(shoulderServo, tiltAngle);
+    ServoController[BASESERVO].setTarget(panAngle);
+    ServoController[SHOULDERSERVO].setTarget(tiltAngle);
+
+
 }
